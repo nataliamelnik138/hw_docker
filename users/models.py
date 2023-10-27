11b2146b53ed96c692c93debf6705387b62ee2c0
Natalia Melnik <nataliamelnik138@gmail.com>
2023-10-27 20:20:23 +0600

    Создана модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    avatar = models.ImageField(
+        upload_to='users/',
+        null=True,
+        blank=True,
+        verbose_name='Аватар'
+    )
+    phone = models.CharField(max_length=35, verbose_name='Телефон', null=True, blank=True)
+    city = models.CharField(max_length=35, verbose_name='Город', null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
