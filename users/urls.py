2e5068f8400bd3f90b9641499ba8d66082503ef6
Natalia Melnik <nataliamelnik138@gmail.com>
2023-10-27 20:36:46 +0600

    Описаны urls

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from users.apps import UsersConfig
+from users.views import UserCreateAPIView, UserUpdateAPIView, UserRetrieveAPIView
+
+app_name = UsersConfig.name
+
+
+urlpatterns = [
+    path('create/', UserCreateAPIView.as_view(), name='user-create'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user-update'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user-get'),
+]
