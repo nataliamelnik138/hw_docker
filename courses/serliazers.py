a145e7424b36c9e57a05d4ccce1821702177b945
Natalia Melnik <nataliamelnik138@gmail.com>
2023-10-27 22:01:49 +0600

    Для модели курса добавлено в сериализатор поле вывода количества уроков

--- courses/serliazers.py
+++ courses/serliazers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from courses.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    number_of_lessons = SerializerMethodField()
+
+    def get_number_of_lessons(self, course):
+        return Lesson.objects.filter(course=course).count()
+
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ('id', 'title', 'description', 'preview_image', 'number_of_lessons',)
 
 
 class LessonSerializer(serializers.ModelSerializer):
