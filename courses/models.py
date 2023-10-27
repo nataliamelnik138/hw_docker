03e6e733b491279aa130f4ecf376fcd5c0fb1439
Natalia Melnik <nataliamelnik138@gmail.com>
2023-10-27 22:18:20 +0600

    Создана модель Payment

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class Course(models.Model):
     title = models.CharField(max_length=150, verbose_name='название')
@@ -37,3 +39,19 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payment')
+    date = models.DateField(verbose_name='дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True, related_name='payment')
+    lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, null=True, blank=True, related_name='payment')
+    amount = models.FloatField(verbose_name='сумма оплаты')
+    method = models.CharField(max_length=150,verbose_name='сумма оплаты')
+
+    def __str__(self):
+        return f'{self.pk} {self.date}'
+
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
