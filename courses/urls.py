9d6e853e7a98bdeacca282f6c75800ef8126711d
Natalia Melnik <nataliamelnik138@gmail.com>
2023-10-28 14:47:48 +0600

    Исправлены urls

--- courses/urls.py
+++ courses/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
     path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
     path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
-    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
-    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
 ] + router.urls
--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('create/', UserCreateAPIView.as_view(), name='user-create'),
-    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user-update'),
+    path('<int:pk>/update/', UserUpdateAPIView.as_view(), name='user-update'),
     path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user-get'),
 ]
