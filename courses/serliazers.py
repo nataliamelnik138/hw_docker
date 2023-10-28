633c14f8b6a44717c3eb07056a1adc8e479158ac
Natalia Melnik <nataliamelnik138@gmail.com>
2023-10-28 15:50:44 +0600

    Добавлен сериализатор для модели Payment, вывод уроков в сериализаторе для модели Course

--- courses/serliazers.py
+++ courses/serliazers.py
@@ -1,21 +1,28 @@
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
-from courses.models import Course, Lesson
+from courses.models import Course, Lesson, Payment
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
 
 
 class CourseSerializer(serializers.ModelSerializer):
     number_of_lessons = SerializerMethodField()
+    lesson = LessonSerializer(many=True, read_only=True)
 
     def get_number_of_lessons(self, course):
         return Lesson.objects.filter(course=course).count()
 
     class Meta:
         model = Course
-        fields = ('id', 'title', 'description', 'preview_image', 'number_of_lessons',)
+        fields = ('id', 'title', 'description', 'preview_image', 'number_of_lessons', 'lesson',)
 
 
-class LessonSerializer(serializers.ModelSerializer):
+class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Lesson
+        model = Payment
         fields = '__all__'
