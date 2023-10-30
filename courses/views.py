6c5c2270a7ab6c40f33376605680ac9259bd33e2
Natalia Melnik <nataliamelnik138@gmail.com>
2023-10-30 21:08:30 +0600

    Настроены права доступа

--- courses/views.py
+++ courses/views.py
@@ -1,44 +1,80 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics
 from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from courses.models import Course, Lesson, Payment
+from courses.permissions import IsModerator, IsOwner
 from courses.serliazers import CourseSerializer, LessonSerializer, PaymentSerializer
+from users.models import UserRoles
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
-    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        new_course = serializer.save()
+        new_course.owner = self.request.user
+        new_course.save()
+
+    def get_queryset(self):
+        if not self.request.user.role == UserRoles.MODERATOR:
+            queryset = Course.objects.filter(owner=self.request.user)
+        else:
+            queryset = Course.objects.all()
+        return queryset
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'create':
+            permission_classes = [IsAuthenticated, ~IsModerator]
+        elif self.action == 'list' or self.action == 'retrieve':
+            permission_classes = [IsAuthenticated, IsModerator | IsOwner]
+        elif self.action == 'update' or self.action == 'destroy':
+            permission_classes = [IsAuthenticated, IsOwner]
+        return [permission() for permission in permission_classes]
 
 
 class LessonCreateAPIView(generics.ListCreateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, ~IsModerator]
+
+    def perform_create(self, serializer):
+        new_lesson = serializer.save()
+        new_lesson.owner = self.request.user
+        new_lesson.save()
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
+
+    def get_queryset(self):
+        if not self.request.user.role == UserRoles.MODERATOR:
+            queryset = Lesson.objects.filter(owner=self.request.user)
+        else:
+            queryset = Lesson.objects.all()
+        return queryset
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
 
 
 class PaymentListAPIView(generics.ListAPIView):
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from users.models import User
-from users.serliazers import UserCreateSerializer, UserSerializer
+from users.permissions import IsActivUser
+from users.serliazers import UserCreateSerializer, UserSerializer, UserPublicSerializer
 
 
 class UserCreateAPIView(generics.CreateAPIView):
@@ -9,15 +11,23 @@ class UserCreateAPIView(generics.CreateAPIView):
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
-    serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def get_serializer_class(self, **kwargs):
+        if self.request.user == User.objects.get(pk=self.kwargs.get('pk')):
+            self.serializer_class = UserSerializer
+        else:
+            self.serializer_class = UserPublicSerializer
+
+        return self.serializer_class
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsActivUser]
 
 
 class UserListAPIView(generics.ListAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserPublicSerializer
     queryset = User.objects.all()
