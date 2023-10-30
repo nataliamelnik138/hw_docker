8ed2f10137a9865a479cf898a52a246055476233
Natalia Melnik <nataliamelnik138@gmail.com>
2023-10-30 21:05:55 +0600

    Добавлены urls для получения и изменения токена

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.apps import UsersConfig
 from users.views import UserCreateAPIView, UserUpdateAPIView, UserRetrieveAPIView, UserListAPIView
@@ -8,6 +11,8 @@ app_name = UsersConfig.name
 
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('create/', UserCreateAPIView.as_view(), name='user-create'),
     path('', UserListAPIView.as_view(), name='user-list'),
     path('<int:pk>/update/', UserUpdateAPIView.as_view(), name='user-update'),
