d5a5a98403d5d5cea745019a936dd6372ca54843
Natalia Melnik <nataliamelnik138@gmail.com>
2023-11-10 23:30:56 +0600

    В CourseSerializer добавлена ссылка на оплату курса

--- courses/serliazers.py
+++ courses/serliazers.py
@@ -1,7 +1,10 @@
+from datetime import date
+
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
 from courses.models import Course, Lesson, Payment, Subscription
+from courses.services import get_a_link_to_pay_for_the_course
 from courses.validators import LinkToVideoValidator
 
 
@@ -18,6 +21,7 @@ class CourseSerializer(serializers.ModelSerializer):
     number_of_lessons = SerializerMethodField()
     lesson = LessonSerializer(many=True, read_only=True)
     signed = SerializerMethodField()
+    payment_link = SerializerMethodField(read_only=True)
 
     def get_number_of_lessons(self, course):
         return Lesson.objects.filter(course=course).count()
@@ -28,9 +32,23 @@ class CourseSerializer(serializers.ModelSerializer):
             return True
         return False
 
+    def get_payment_link(self, course):
+        user = self.context['request'].user
+        current_date = date.today()
+        Payment.objects.create(
+            user=user,
+            date=current_date,
+            course=course,
+            amount=course.price,
+            method='non-cash'
+        )
+
+        return get_a_link_to_pay_for_the_course(course)
+
     class Meta:
         model = Course
-        fields = ('id', 'title', 'description', 'preview_image', 'number_of_lessons', 'owner', 'lesson', 'signed',)
+        fields = ('id', 'title', 'description', 'preview_image', 'number_of_lessons', 'owner', 'lesson', 'signed',
+                  'payment_link',)
 
 
 class PaymentSerializer(serializers.ModelSerializer):
